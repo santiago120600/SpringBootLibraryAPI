88ea3c17000da5e10bd9bcbc5475ffb017af457d
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-06-19 12:42:32 -0600

    refactor: remove unnesessary custom repository

--- src/main/java/com/api/library/repository/LibraryRepository.java
+++ src/main/java/com/api/library/repository/LibraryRepository.java
@@ -3,7 +3,8 @@ package com.api.library.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.library.model.Library;
+import java.util.List;
 
-public interface LibraryRepository extends JpaRepository<Library, String>, LibraryRepositoryCustom {
-
+public interface LibraryRepository extends JpaRepository<Library, String> {
+    List<Library> findByAuthor(String author);
 }
