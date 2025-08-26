f494fb4e58c846001872ddc5796254b10b037519
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-08-26 16:50:22 -0600

    fix: update response field names

--- src/main/java/com/api/library/dto/Pagination.java
+++ src/main/java/com/api/library/dto/Pagination.java
@@ -1,11 +1,17 @@
 package com.api.library.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class Pagination {
+    @JsonProperty("current_page")
     private int currentPage;
+    @JsonProperty("per_page")
     private int pageSize;
+    @JsonProperty("total")
     private long totalElements;
+    @JsonProperty("total_pages")
     private int totalPages;
 }
