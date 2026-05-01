f70ffe52fcb273a6b9ed7d7d979e80ab39b66f76
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2026-05-01 17:56:43 -0600

    feat: enhance BookControllerTests with additional test cases for CRUD operations

--- src/test/java/com/api/library/controller/v1/BookControllerTests.java
+++ src/test/java/com/api/library/controller/v1/BookControllerTests.java
@@ -3,7 +3,13 @@ package com.api.library.controller.v1;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -20,7 +26,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.api.library.dto.AuthorRequest;
 import com.api.library.dto.AuthorResponse;
 import com.api.library.dto.BookRequest;
 import com.api.library.dto.BookResponse;
@@ -104,6 +109,90 @@ public class BookControllerTests {
                                 .andExpect(MockMvcResultMatchers.jsonPath("$.title").value(bookRequest.getTitle()))
                                 .andExpect(MockMvcResultMatchers.jsonPath("$.isbn").value(bookRequest.getIsbn()))
                                 .andExpect(MockMvcResultMatchers.jsonPath("$.aisle_number")
-                                                .value(bookRequest.getAisleNumber()));
+                                                .value(bookRequest.getAisleNumber()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.id").value(AUTHOR_ID))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.first_name")
+                                                .value(authorResponse.getFirstName()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.last_name")
+                                                .value(authorResponse.getLastName()));
         }
+
+        @Test
+        public void BookController_UpdateBook_ReturnOk() throws JsonProcessingException, Exception {
+                given(bookService.updateBook(eq(BOOK_ID), any(BookRequest.class)))
+                                .willReturn(bookResponse);
+
+                ResultActions result = mockMvc.perform(
+                                put("/api/v1/books/{id}", BOOK_ID)
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(bookRequest)));
+
+                result.andExpect(MockMvcResultMatchers.status().isOk())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(BOOK_ID))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value(bookRequest.getTitle()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.isbn").value(bookRequest.getIsbn()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.aisle_number")
+                                                .value(bookRequest.getAisleNumber()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.id").value(AUTHOR_ID))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.first_name")
+                                                .value(authorResponse.getFirstName()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.last_name")
+                                                .value(authorResponse.getLastName()));
+        }
+        
+        @Test
+        public void BookController_GetBooks_ReturnOk() throws Exception {
+                given(bookService.getAllBooks(any(BookRequest.BookRequestBuilder.class)))
+                                .willReturn(List.of(bookResponse));
+
+                ResultActions result = mockMvc.perform(
+                                get("/api/v1/books")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .param("title", TITLE)
+                                                .param("isbn", ISBN)
+                                                .param("aisleNumber", String.valueOf(AISLE_NUMBER)));
+
+                result.andExpect(MockMvcResultMatchers.status().isOk())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(BOOK_ID))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value(bookResponse.getTitle()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$[0].isbn").value(bookResponse.getIsbn()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$[0].aisle_number")
+                                                .value(bookResponse.getAisleNumber()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$[0].author.id").value(AUTHOR_ID))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$[0].author.first_name")
+                                                .value(authorResponse.getFirstName()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$[0].author.last_name")
+                                                .value(authorResponse.getLastName()));
+        }
+        
+        @Test
+        public void BookController_GetBookById_ReturnOk() throws Exception {
+                given(bookService.getBookById(eq(BOOK_ID))).willReturn(bookResponse);
+
+                ResultActions result = mockMvc.perform(
+                                get("/api/v1/books/{id}", BOOK_ID)
+                                                .contentType(MediaType.APPLICATION_JSON));
+
+                result.andExpect(MockMvcResultMatchers.status().isOk())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(BOOK_ID))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value(bookResponse.getTitle()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.isbn").value(bookResponse.getIsbn()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.aisle_number")
+                                                .value(bookResponse.getAisleNumber()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.id").value(AUTHOR_ID))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.first_name")
+                                                .value(authorResponse.getFirstName()))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.author.last_name")
+                                                .value(authorResponse.getLastName()));
+        }
+
+        @Test
+        public void BookController_DeleteBook_ReturnNoContent() throws Exception {
+                doNothing().when(bookService).deleteBook(eq(BOOK_ID));
+                ResultActions result = mockMvc.perform(
+                                delete("/api/v1/books/{id}", BOOK_ID)
+                                                .contentType(MediaType.APPLICATION_JSON));
+                result.andExpect(MockMvcResultMatchers.status().isNoContent());
+        }
+
 }
