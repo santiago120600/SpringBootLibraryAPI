58c68c060fbb8fb024326cab3e1e32240ca48949
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2026-05-01 15:46:53 -0600

    feat: expand BookServiceTests with additional test cases and improve assertions

--- src/test/java/com/api/library/service/v1/BookServiceTests.java
+++ src/test/java/com/api/library/service/v1/BookServiceTests.java
@@ -1,8 +1,15 @@
 package com.api.library.service.v1;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +19,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.api.library.dto.BookRequest;
 import com.api.library.dto.BookResponse;
@@ -82,8 +91,8 @@ public class BookServiceTests {
     @Test
     public void BookService_CreateBook_ReturnsBookDto() {
         when(bookRepository.findByIsbn(ISBN)).thenReturn(null);
-        when(authorRepository.findById(AUTHOR_ID)).thenReturn(java.util.Optional.of(author));
-        when(bookRepository.save(Mockito.any(Book.class))).thenReturn(book);
+        when(authorRepository.findById(AUTHOR_ID)).thenReturn(Optional.of(author));
+        when(bookRepository.save(any(Book.class))).thenReturn(book);
 
         BookResponse savedBook = bookService.addBook(bookRequest, AUTHOR_ID);
         Assertions.assertNotNull(savedBook);
@@ -94,7 +103,117 @@ public class BookServiceTests {
         Assertions.assertEquals(book.getAuthor().getLastName(), savedBook.getAuthor().getLastName());
         verify(authorRepository, times(1)).findById(AUTHOR_ID);
         verify(bookRepository, times(1)).findByIsbn(ISBN);
-        verify(bookRepository, times(1)).save(Mockito.any(Book.class));
+        verify(bookRepository, times(1)).save(any(Book.class));
     }
 
+    @Test
+    public void BookService_CreateBook_ReturnsExistingBook_WhenBookAlreadyExists() {
+        when(bookRepository.findByIsbn(ISBN)).thenReturn(book);
+
+        BookResponse savedBook = bookService.addBook(bookRequest, AUTHOR_ID);
+        Assertions.assertNotNull(savedBook);
+        Assertions.assertEquals(book.getTitle(), savedBook.getTitle());
+        Assertions.assertEquals(book.getIsbn(), savedBook.getIsbn());
+        Assertions.assertEquals(book.getAisleNumber(), savedBook.getAisleNumber());
+        Assertions.assertEquals(book.getAuthor().getFirstName(), savedBook.getAuthor().getFirstName());
+        Assertions.assertEquals(book.getAuthor().getLastName(), savedBook.getAuthor().getLastName());
+        verify(bookRepository, times(1)).findByIsbn(ISBN);
+        verify(authorRepository, times(0)).findById(AUTHOR_ID);
+        verify(bookRepository, times(0)).save(any(Book.class));
+    }
+
+    @Test
+    public void BookService_GetAllBooks_WithTitle_ShouldReturnFilteredBooks() {
+        String title = "The Great Gatsby";
+
+        BookRequest.BookRequestBuilder filterBuilder = BookRequest.builder().title(title);
+
+        when(bookRepository.findAll(any(Specification.class))).thenReturn(Arrays.asList(book));
+
+        List<BookResponse> result = bookService.getAllBooks(filterBuilder);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals(title, result.get(0).getTitle());
+        Assertions.assertEquals(book.getIsbn(), result.get(0).getIsbn());
+        Assertions.assertEquals(book.getAisleNumber(), result.get(0).getAisleNumber());
+        Assertions.assertEquals(book.getAuthor().getFirstName(), result.get(0).getAuthor().getFirstName());
+        Assertions.assertEquals(book.getAuthor().getLastName(), result.get(0).getAuthor().getLastName());
+        verify(bookRepository, times(1)).findAll(any(Specification.class));
+    }
+
+    @Test
+    public void BookService_GetBookById_ReturnsBookDto() {
+        when(bookRepository.findById(AUTHOR_ID)).thenReturn(Optional.of(book));
+        BookResponse foundBook = bookService.getBookById(AUTHOR_ID);
+        Assertions.assertNotNull(foundBook);
+        Assertions.assertEquals(book.getTitle(), foundBook.getTitle());
+        Assertions.assertEquals(book.getIsbn(), foundBook.getIsbn());
+        Assertions.assertEquals(book.getAisleNumber(), foundBook.getAisleNumber());
+        Assertions.assertEquals(book.getAuthor().getFirstName(), foundBook.getAuthor().getFirstName());
+        Assertions.assertEquals(book.getAuthor().getLastName(), foundBook.getAuthor().getLastName());
+        verify(bookRepository, times(1)).findById(AUTHOR_ID);
+    }
+
+    @Test
+    public void BookService_GetBookById_ThrowNotFound_WhenBookDoesNotExist() {
+        when(bookRepository.findById(AUTHOR_ID)).thenReturn(Optional.empty());
+
+        ResponseStatusException exception = Assertions.assertThrows(
+                ResponseStatusException.class,
+                () -> bookService.getBookById(AUTHOR_ID));
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+        Assertions.assertEquals("404 NOT_FOUND", exception.getMessage());
+
+        verify(bookRepository, times(1)).findById(AUTHOR_ID);
+    }
+
+    @Test
+    public void BookService_UpdateBook_ReturnsBookDto() {
+        when(bookRepository.findById(AUTHOR_ID)).thenReturn(Optional.of(book));
+        when(authorRepository.findById(AUTHOR_ID)).thenReturn(Optional.of(author));
+        when(bookRepository.save(any(Book.class))).thenReturn(book);
+        BookResponse updatedBook = bookService.updateBook(AUTHOR_ID, bookRequest);
+        Assertions.assertNotNull(updatedBook);
+        Assertions.assertEquals(book.getTitle(), updatedBook.getTitle());
+        Assertions.assertEquals(book.getIsbn(), updatedBook.getIsbn());
+        Assertions.assertEquals(book.getAisleNumber(), updatedBook.getAisleNumber());
+        Assertions.assertEquals(book.getAuthor().getFirstName(), updatedBook.getAuthor().getFirstName());
+        Assertions.assertEquals(book.getAuthor().getLastName(), updatedBook.getAuthor().getLastName());
+    }
+
+    @Test
+    public void BookService_UpdateBook_ThrowNotFound_WhenBookDoesNotExist() {
+        when(bookRepository.findById(AUTHOR_ID)).thenReturn(Optional.empty());
+
+        ResponseStatusException exception = Assertions.assertThrows(
+                ResponseStatusException.class,
+                () -> bookService.updateBook(AUTHOR_ID, bookRequest));
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+        Assertions.assertEquals("404 NOT_FOUND", exception.getMessage());
+
+        verify(bookRepository, times(1)).findById(AUTHOR_ID);
+        verify(bookRepository, times(0)).save(any(Book.class));
+    }
+
+    @Test
+    public void BookService_DeleteBook_ReturnsVoid() {
+        when(bookRepository.findById(AUTHOR_ID)).thenReturn(Optional.of(book));
+        doNothing().when(bookRepository).delete(book);
+        assertAll(() -> bookService.deleteBook(AUTHOR_ID));
+    }
+
+    @Test
+    public void BookService_DeleteBook_ThrowNotFound_WhenBookDoesNotExist() {
+        when(bookRepository.findById(AUTHOR_ID)).thenReturn(Optional.empty());
+
+        ResponseStatusException exception = Assertions.assertThrows(
+                ResponseStatusException.class,
+                () -> bookService.deleteBook(AUTHOR_ID));
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+        Assertions.assertEquals("404 NOT_FOUND", exception.getMessage());
+
+        verify(bookRepository, times(1)).findById(AUTHOR_ID);
+        verify(bookRepository, times(0)).delete(any(Book.class));
+    }
 }
